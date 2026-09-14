46e298ba602c4044b335175ffb5acabc5e2e3829
Patamawadee Ngampradit <patamawadee.ng@gmail.com>
2026-09-14 21:38:58 +0700

    Q2

---
+++ SQL_Project/2_top_paying_job_skill.sql
@@ -0,0 +1,275 @@
+/*
+Question: What are the top-paying data scientist jobs, and what skills are required? 
+- Identify the top 10 highest-paying Data Scientist jobs and the specific skills required for these roles.
+- Filters for roles with specified salaries that are remote
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, helping job seekers understand which skills to develop that align with top salaries
+*/
+
+-- Step 1: Filter and get only the top 10 highest-paying remote Data Scientist jobs first to reduce data size
+WITH top_10_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        company_id,
+        salary_year_avg
+    FROM
+        job_postings_fact
+    WHERE
+        job_title_short = 'Data Scientist'
+        AND job_location = 'Anywhere'
+        AND salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+-- Step 2: Join the filtered top 10 jobs with company and skills tables to get company names and required skills
+SELECT
+    j.job_id,
+    j.job_title,
+    j.salary_year_avg,
+    c.name AS company_name,
+    s.skills AS skill_required
+FROM
+    top_10_jobs AS j
+    LEFT JOIN company_dim AS c ON j.company_id = c.company_id
+    INNER JOIN skills_job_dim AS sj ON j.job_id = sj.job_id
+    INNER JOIN skills_dim AS s ON sj.skill_id = s.skill_id
+ORDER BY
+    salary_year_avg DESC;
+
+/*
+Here’s the breakdown of the most demanded skills for Data Scientists in 2023, based on the top-paying job postings:
+• Python is leading with a count of 5.
+• SQL follows closely with a count of 4.
+• AWS is also highly sought after, with a count of 3.
+• Java and GCP each appear with a count of 2.
+• Other skills like Cassandra, Hadoop, Spark, Tableau, Azure, TensorFlow, Keras, PyTorch, Scikit-learn, DataRobot, C, Pandas, and NumPy each have a count of 1.
+
+[
+  {
+    "job_id": 40145,
+    "job_title": "Staff Data Scientist/Quant Researcher",
+    "salary_year_avg": "550000.0",
+    "company_name": "Selby Jennings",
+    "skill_required": "sql"
+  },
+  {
+    "job_id": 40145,
+    "job_title": "Staff Data Scientist/Quant Researcher",
+    "salary_year_avg": "550000.0",
+    "company_name": "Selby Jennings",
+    "skill_required": "python"
+  },
+  {
+    "job_id": 1714768,
+    "job_title": "Staff Data Scientist - Business Analytics",
+    "salary_year_avg": "525000.0",
+    "company_name": "Selby Jennings",
+    "skill_required": "sql"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "salary_year_avg": "375000.0",
+    "company_name": "Algo Capital Group",
+    "skill_required": "sql"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "salary_year_avg": "375000.0",
+    "company_name": "Algo Capital Group",
+    "skill_required": "python"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "salary_year_avg": "375000.0",
+    "company_name": "Algo Capital Group",
+    "skill_required": "java"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "salary_year_avg": "375000.0",
+    "company_name": "Algo Capital Group",
+    "skill_required": "cassandra"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "salary_year_avg": "375000.0",
+    "company_name": "Algo Capital Group",
+    "skill_required": "spark"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "salary_year_avg": "375000.0",
+    "company_name": "Algo Capital Group",
+    "skill_required": "hadoop"
+  },
+  {
+    "job_id": 1131472,
+    "job_title": "Data Scientist",
+    "salary_year_avg": "375000.0",
+    "company_name": "Algo Capital Group",
+    "skill_required": "tableau"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "salary_year_avg": "320000.0",
+    "company_name": "Teramind",
+    "skill_required": "azure"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "salary_year_avg": "320000.0",
+    "company_name": "Teramind",
+    "skill_required": "aws"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "salary_year_avg": "320000.0",
+    "company_name": "Teramind",
+    "skill_required": "tensorflow"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "salary_year_avg": "320000.0",
+    "company_name": "Teramind",
+    "skill_required": "keras"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "salary_year_avg": "320000.0",
+    "company_name": "Teramind",
+    "skill_required": "pytorch"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "salary_year_avg": "320000.0",
+    "company_name": "Teramind",
+    "skill_required": "scikit-learn"
+  },
+  {
+    "job_id": 126218,
+    "job_title": "Director Level - Product Management - Data Science",
+    "salary_year_avg": "320000.0",
+    "company_name": "Teramind",
+    "skill_required": "datarobot"
+  },
+  {
+    "job_id": 38905,
+    "job_title": "Principal Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Storm5",
+    "skill_required": "sql"
+  },
+  {
+    "job_id": 38905,
+    "job_title": "Principal Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Storm5",
+    "skill_required": "python"
+  },
+  {
+    "job_id": 38905,
+    "job_title": "Principal Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Storm5",
+    "skill_required": "java"
+  },
+  {
+    "job_id": 38905,
+    "job_title": "Principal Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Storm5",
+    "skill_required": "c"
+  },
+  {
+    "job_id": 38905,
+    "job_title": "Principal Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Storm5",
+    "skill_required": "aws"
+  },
+  {
+    "job_id": 38905,
+    "job_title": "Principal Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Storm5",
+    "skill_required": "gcp"
+  },
+  {
+    "job_id": 226011,
+    "job_title": "Distinguished Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Walmart",
+    "skill_required": "scala"
+  },
+  {
+    "job_id": 226011,
+    "job_title": "Distinguished Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Walmart",
+    "skill_required": "java"
+  },
+  {
+    "job_id": 226011,
+    "job_title": "Distinguished Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Walmart",
+    "skill_required": "spark"
+  },
+  {
+    "job_id": 226011,
+    "job_title": "Distinguished Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Walmart",
+    "skill_required": "tensorflow"
+  },
+  {
+    "job_id": 226011,
+    "job_title": "Distinguished Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Walmart",
+    "skill_required": "pytorch"
+  },
+  {
+    "job_id": 226011,
+    "job_title": "Distinguished Data Scientist",
+    "salary_year_avg": "300000.0",
+    "company_name": "Walmart",
+    "skill_required": "kubernetes"
+  },
+  {
+    "job_id": 457991,
+    "job_title": "Head of Battery Data Science",
+    "salary_year_avg": "300000.0",
+    "company_name": "Lawrence Harvey",
+    "skill_required": "python"
+  },
+  {
+    "job_id": 457991,
+    "job_title": "Head of Battery Data Science",
+    "salary_year_avg": "300000.0",
+    "company_name": "Lawrence Harvey",
+    "skill_required": "aws"
+  },
+  {
+    "job_id": 457991,
+    "job_title": "Head of Battery Data Science",
+    "salary_year_avg": "300000.0",
+    "company_name": "Lawrence Harvey",
+    "skill_required": "gcp"
+  }
+]
+*/
